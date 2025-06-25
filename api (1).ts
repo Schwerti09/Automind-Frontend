5c265a5ab911324d6be95e20892b0b5212af9609
Schwerti09 <rps-vertrieb@t-online.de>
2025-06-25 16:31:39 +0200

    Add files via upload

---
+++ api (1).ts
@@ -0,0 +1,11 @@
+export async function sendRequest(endpoint: string, data: any) {
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  return res.json();
+}
