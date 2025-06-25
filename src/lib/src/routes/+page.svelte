bc9485e9f5ee129065b1a192a78cd73d2a4836bf
Schwerti09 <rps-vertrieb@t-online.de>
2025-06-25 18:32:59 +0200

    Create +page.svelte

---
+++ src/lib/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- src/routes/+page.svelte -->
+<script>
+	import { pingServer } from '$lib/api.ts';
+	
+	pingServer();
+</script>
+
+<main>
+	<h1>🤖 Willkommen bei AUTOMIND</h1>
+	<p>Frontend erfolgreich verbunden.</p>
+</main>
